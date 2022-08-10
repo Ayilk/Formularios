aea128a107eb068e70b34ba60fc2a34347441874
Ayilk <srangel.h92@gmail.com>
2022-08-10 18:07:20 -0500

    Evaluar un email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -11,14 +11,20 @@ export class RegistroComponent implements OnInit {
 
   //Temporal
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z09._%+-]+@[a-z09.-]\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]]
   })
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: "Sandra Rangel",
+      email: 'test1@mail.com'
+    })
   }
 
   campoNoValido( campo:string ){
