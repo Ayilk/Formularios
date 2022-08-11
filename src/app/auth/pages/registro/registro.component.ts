2b8236eeb98f2be81bc171276ac940e55cb2d726
Ayilk <srangel.h92@gmail.com>
2022-08-11 00:50:59 -0500

    Mensaje de error personalizado

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -20,7 +20,26 @@ export class RegistroComponent implements OnInit {
     password2: ['', [Validators.required]]
   },{
     validators: [this.validatorService.camposIguales('password', 'password2')]
-  })
+  });
+
+  //emailErrorMsg: string='';
+
+  
+  get emailErrorMsg():string{
+    //console.log('tick');
+    //return 'Hola Mundo'
+
+    const errors = this.miFormulario.get('email')?.errors;
+    if(errors?.['required']){
+      return 'Email es obligatorio'
+    }else if(errors?.['pattern']){///Hay un error con el pattern
+      return 'El valor ingresado no tiene formato de correo'
+    }else if(errors?.['emailTomado']){
+      return 'El email ya fue tomado'
+    }
+
+    return '';
+  }
 
   constructor(private fb: FormBuilder,
               private validatorService: ValidatorsService,
@@ -41,21 +60,7 @@ export class RegistroComponent implements OnInit {
            this.miFormulario.get(campo)?.touched
   }
 
-  emailRequired(){
-    return this.miFormulario.get('email')?.invalid &&
-           this.miFormulario.get('email')?.touched
-  }
-
-  emailFromato(){
-    return this.miFormulario.get('email')?.errors?['pattern'] &&
-           this.miFormulario.get('email')?.touched
-  }
-
-  emailTomado(){
-    return this.miFormulario.get('email')?.errors?.['emailTomado'] &&
-           this.miFormulario.get('email')?.touched
-  }
-
+ 
 
   submitFormulario(){
     console.log(this.miFormulario.value);
